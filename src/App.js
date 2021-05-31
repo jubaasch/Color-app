01dc8ee24bf73e8241e00f4b02d8207b52b38ee1
Julia Baasch <juliabaaschw@gmail.com>
2021-05-31 15:50:43 -0300

    add colorHelpers to generate new palette

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 function App() {
+  console.log(generatePalette(seedColors[0]))
   return (
     <div className="App">
       <Palette {...seedColors[4]} />
