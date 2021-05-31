0236818a610504e08683e4901e5695a4d9df123d
Julia Baasch <juliabaaschw@gmail.com>
2021-05-31 14:32:06 -0300

    style ColorBox, add uuid

--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import ColorBox from './ColorBox';
 import './Palette.css';
 
 class Palette extends Component {
     render() {
         const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name} />
+            <ColorBox background={color.color} name={color.name} key={uuidv4()} />
         ))
         return (
             <div className="Palette">
