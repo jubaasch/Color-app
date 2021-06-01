970741a933228effd475aacd417700d899aaceff
Julia Baasch <juliabaaschw@gmail.com>
2021-06-01 15:20:40 -0300

    add SingleColorPalette and link

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
-import './App.css';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
+import './App.css';
 
 function findPalette(id) {
   return seedColors.find(function (palette) {
@@ -17,17 +18,20 @@ function App() {
       <Route
         exact
         path="/"
-        render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
+        render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />}
+      />
       <Route
         exact
         path="/palette/:id"
         render={(routeProps) => <Palette
-          palette={generatePalette(findPalette(routeProps.match.params.id))} />} />
+          palette={generatePalette(findPalette(routeProps.match.params.id))} />}
+      />
+      <Route
+        exact
+        path="/palette/:paletteId/:colorId"
+        render={() => <SingleColorPalette />}
+      />
     </Switch>
-
-    /* <div className="App">
-      <Palette palette={generatePalette(seedColors[0])} />
-    </div> */
   );
 }
 
