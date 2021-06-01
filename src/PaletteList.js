519dbef492a9a6d363113c785e14f45a54574456
Julia Baasch <juliabaaschw@gmail.com>
2021-06-01 14:39:35 -0300

    add more styling to MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,7 +5,7 @@ import MiniPallete from './MiniPalette';
 const styles = {
     root: {
         backgroundColor: 'blue',
-        height: '100%',
+        height: '100vh',
         display: 'flex',
         alignItems: 'flex-start',
         justifyContent: 'center'
