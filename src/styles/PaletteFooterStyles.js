e097185cf65f94993162b6ed98968e55181dccf6
Julia Baasch <juliabaaschw@gmail.com>
2021-06-02 14:08:37 -0300

    move styles to separate files

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,15 @@
+const styles = {
+    footer: {
+        backgroundColor: 'white',
+        height: '4vh',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        fontWeight: 'bold'
+    },
+    emoji: {
+        margin: '0 1rem'
+    }
+}
+
+export default styles;
